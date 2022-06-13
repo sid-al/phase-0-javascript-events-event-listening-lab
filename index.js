31a22476f8b1f25994d1415c90ee6915c7131953
sid-al <sendplusx@gmail.com>
2022-06-13 19:10:53 +0300

    commit tree 5e7f29d8d75f6231bf7d70c43b3ccd0e8ea63bfc

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ input.addEventListener('click', function() {
   alert('I was clicked!');
 });
 }
+
+const input = docement.getElementById('input');
+function clickAlert(){
+  alert('i was clicked')
+};
+
+Input.addEventListener('click', clickAlert);
+
+
